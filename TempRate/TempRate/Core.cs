b6089d83b7d86776e3d58ef127611f009bf2b0cb
Eli-Frazier <42358273+Eli-Frazier@users.noreply.github.com>
2019-01-05 04:02:28 -0500

    01-05-19

---
+++ TempRate/TempRate/Core.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempRate
+{
+    class Core
+    {
+        public static int CalculateRate(TimeSpan srtTime, int srtTemp, TimeSpan endTime, int endTemp)
+        {
+            var timeOne = Convert.ToInt32(srtTime);
+            var timeTwo = Convert.ToInt32(endTime);
+            int Rate = (timeOne - srtTemp) / (timeTwo - endTemp);
+
+            return Rate;
+        }
+    }
+}
--- TempRate/TempRate/MainPage.xaml.cs
+++ TempRate/TempRate/MainPage.xaml.cs
@@ -12,6 +12,30 @@ namespace TempRate
         public MainPage()
         {
             InitializeComponent();
+
+            FindTempRatebtn.Clicked += FindTempRatebtn_Clicked;
+
+            Title = "Enter Data Here";
+        }
+
+        private void FindTempRatebtn_Clicked(object sender, EventArgs e)
+        {
+            var timeStart = Convert.ToDateTime(srtTime);
+            var tempStart = int.Parse(srtTemp.Text);
+
+            var timeEnd = Convert.ToString(endTime).Split(' ')[0];
+            var tempEnd = int.Parse(endTemp.Text);
+
+            int result = Core.CalculateRate(timeStart, tempStart, timeEnd, tempEnd);
+
+            if((tempStart > 70 && result >= .542) || (tempStart < 70 && result >= .121))
+            {
+                DisplayAlert("Success", $"the rate of change is {result} deg/min amd passes", "OK");
+            }
+            else
+            {
+                DisplayAlert("Failure", $"The rate of change is {result} deg/min and fails", "OK");
+            }
         }
     }
 }
